202c6f9d3a1dda0744f1ffd06703af17db15f844
Antonio Josué Gaitán Quesada <josue.qu12@gmail.com>
2021-03-02 01:33:53 +0000

    files added

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
+import "../../styles/style.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center">Simple Counter</h1>
+			<div className="row d-flex justify-content-around text-center">
+				<div className="col box">
+					<i className="far fa-clock"></i>
+				</div>
+				<div className="col box">{props.number6}</div>
+				<div className="col box">{props.number5}</div>
+				<div className="col box">{props.number4}</div>
+				<div className="col box">{props.number3}</div>
+				<div className="col box">{props.number2}</div>
+				<div className="col box">{props.number1}</div>
+			</div>
 		</div>
 	);
 }
+
+Home.propTypes = {
+	number1: PropTypes.string,
+	number2: PropTypes.string,
+	number3: PropTypes.string,
+	number4: PropTypes.string,
+	number5: PropTypes.string,
+	number6: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include bootstrap npm library into the bundle
 import "bootstrap";
-
-//include your index.scss file into the bundle
 import "../styles/index.scss";
-
-//import your own components
 import { Home } from "./component/home.js";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let n = 0;
+setInterval(() => {
+	let num = String(n++);
+	ReactDOM.render(
+		<Home
+			number6={num[num.length - 6]}
+			number5={num[num.length - 5]}
+			number4={num[num.length - 4]}
+			number3={num[num.length - 3]}
+			number2={num[num.length - 2]}
+			number1={num[num.length - 1]}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
